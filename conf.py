33ae3c45808f0e80c45834c80094c608db182e77
Ehsan Abdi <ehs.abdi@gmail.com>
2024-03-16 20:32:17 +0100

    initial commit

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ author = 'Ehsan Abdi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['python_docs_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
